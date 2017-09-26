41b13cd86f91757c1e1bed587784a34280e414d2
Conor McGrath <g00291461@gmit.ie>
2017-09-26 13:21:11 +0100

    03-fizzbuzz

---
+++ 03-fizzbuzz.go
@@ -0,0 +1,22 @@
+package main
+
+	import "fmt"
+
+	func main() {
+	fmt.Println("Welcome to fizzbuzz!")
+	c := make([]int, 100)
+	for i := range c {
+		d := i + 1
+		threes := d%3 == 0
+		fives := d%5 == 0
+		if threes && fives {
+			fmt.Println("FizzBuzz")
+		} else if threes {
+			fmt.Println("Fizz")
+		} else if fives {
+			fmt.Println("Buzz")
+		} else {
+			fmt.Println(d)
+		}
+	}
+	}
