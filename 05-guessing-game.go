f09dcb20129446ddf5005a01308847f78390c710
g00291461 <g00291461@gmit.ie>
2017-10-02 19:00:28 +0100

    Guessing Game

--- 05-guessing-game.go
+++ 05-guessing-game.go
@@ -1,9 +1,33 @@
 package main
 
 import "fmt"
+import "math/rand"
+import "time"
 
-func main(){
+func main() {
+	fmt.Print("Welcome to the Guessing Game\n")
+	var guessCount int
+	rand.Seed(time.Now().UTC().UnixNano())
+	var answer = rand.Intn(100)
+	fmt.Print("Enter a number between 1 & 100.\n")
 
-	
+    fmt.Println("Enter a number: ")
+    var guess int
+	fmt.Scanln(&guess)
+	for guess != answer{
+		if guess > answer{
+			fmt.Println("Incorrect. Lower.")
+			guessCount++
+			fmt.Scanln(&guess)
+		} else if guess < answer{
+			fmt.Println("Incorrect. Higher.")
+			guessCount++
+			fmt.Scanln(&guess)
+		} else if guess == answer{
+			return
+		}
+	}
+	fmt.Println("Correct!!")
+	fmt.Printf("Number of guesses: %d", guessCount)
 
 }
