f3077207d15399589b0ff83a84d77f09d00daf4d
g00291461 <g00291461@gmit.ie>
2017-10-03 13:41:24 +0100

    03-fizzbuzz

--- 03-fizzbuzz.go
+++ 03-fizzbuzz.go
@@ -18,7 +18,9 @@ package main
 			threes := d%3 == 0
 			//calculate if variable d is divisible by 5
 			fives := d%5 == 0
-
+			
+			//compare int d against variables threes and fives
+			//prints word according to condition met in if else statement
 			if threes && fives {
 				fmt.Println("FizzBuzz")
 			} else if threes {
